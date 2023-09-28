4f25f2af85359054e3f2423727d2e63e011d9f8f
Tolushawlar <tolushaw.it@gmail.com>
2023-09-28 13:36:06 +0100

    middleware routes included

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,10 +4,18 @@ const userController = require("../controllers/users.controller");
 const authenticate = require("../middleware/authenticate.middleware");
 
 router.get("/api/users", authenticate, userController.getUser);
-router.post("/api/users", userController.createUser);
-router.get("/api/users/:username", userController.getUserDetails);
-router.put("/api/users/:username", userController.updateUserDetails);
-router.delete("/api/users/:username", userController.deleteUserByUsername);
-router.post("/api/user/login", userController.userLogin);
+router.post("/api/users", authenticate, userController.createUser);
+router.get("/api/users/:username", authenticate, userController.getUserDetails);
+router.put(
+  "/api/users/:username",
+  authenticate,
+  userController.updateUserDetails
+);
+router.delete(
+  "/api/users/:username",
+  authenticate,
+  userController.deleteUserByUsername
+);
+router.post("/api/user/login", authenticate, userController.userLogin);
 
 module.exports = router;
